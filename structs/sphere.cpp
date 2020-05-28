4c1fff5968b0b20fa7e7aa6a7770bc7fa62ab38b
Nikolay Danailov <frostblooded@yahoo.com>
2020-05-28 11:44:20 +0300

    Fix mistaken sphere hit algorithm

--- structs/sphere.cpp
+++ structs/sphere.cpp
@@ -4,9 +4,9 @@ bool sphere::hit(const ray &r, float t_min, float t_max, hit_record &rec) const
 {
     vec3 oc = r.origin() - center;
     float a = r.direction().squared_length();
-    float b = 2.0 * oc.dot(r.direction());
+    float b = oc.dot(r.direction());
     float c = oc.squared_length() - radius * radius;
-    float discriminant = b * b - 4 * a * c;
+    float discriminant = b * b - a * c;
 
     if (discriminant > 0)
     {
